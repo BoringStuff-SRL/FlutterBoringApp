f91640e7cf48748260757bbb72c239d78f565e61
besim <enzode.simone@outlook.it>
2023-02-17 14:03:27 +0100

    - Fix drawer icon

--- lib/boring_app/boring_section.dart
+++ lib/boring_app/boring_section.dart
@@ -1,28 +1,33 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
 
+import 'package:boring_app/boring_app/style/boring_drawer_style.dart';
 import 'package:boring_app/boring_app/boring_page/boring_page.dart';
+import 'package:boring_app/boring_app/style/boring_drawer_tile_style.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-import 'package:boring_app/boring_app/boring_page/boring_page_base.dart';
-
 class BoringSection {
   final String? path;
-  List<BoringPageBase> children;
   final Widget Function(BuildContext context)? drawerHeaderBuilder;
   final Widget Function(BuildContext context)? drawerFooterBuilder;
   final String? defaultPath;
+  final BoringDrawerStyle drawerStyle;
+  final BoringDrawerTileStyle drawerTileStyle;
   final FutureOr<String?> Function(BuildContext context, GoRouterState state)?
       redirect;
-  BoringSection({
-    this.path,
-    required this.children,
-    this.drawerHeaderBuilder,
-    this.drawerFooterBuilder,
-    this.defaultPath,
-    this.redirect,
-  }) {
+  final GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
+  List<BoringPageBase> children;
+
+  BoringSection(
+      {this.path,
+      required this.children,
+      this.drawerHeaderBuilder,
+      this.drawerFooterBuilder,
+      this.defaultPath,
+      this.redirect,
+      this.drawerStyle = const BoringDrawerStyle(),
+      this.drawerTileStyle = const BoringDrawerTileStyle()}) {
     _assertions();
   }
 
@@ -60,9 +65,6 @@ class BoringSection {
 
   late final BoringPage? noPathPage;
 
-  final drawerShape =
-      RoundedRectangleBorder(borderRadius: BorderRadius.circular(16));
-  final drawerElevation = 20.0;
   drawerWrap(Widget child) => Card(
         margin: EdgeInsets.zero,
         elevation: 0,
@@ -71,19 +73,21 @@ class BoringSection {
         child: child,
       );
 
-  Drawer drawer(BuildContext context) => Drawer(
-        shape: drawerShape,
-        elevation: drawerElevation,
+  Drawer drawer(BuildContext context, {bool? drawerShow}) => Drawer(
+        shape: drawerShow != null
+            ? null
+            : RoundedRectangleBorder(borderRadius: drawerStyle.drawerRadius),
+        elevation: drawerStyle.drawerElevation,
         child: drawerWrap(Column(
           children: [
             if (drawerHeaderBuilder != null) drawerHeaderBuilder!(context),
             Expanded(
               child: SingleChildScrollView(
-                padding: const EdgeInsets.all(8.0),
+                padding: drawerStyle.drawerContentPadding,
                 child: Column(
                   children: children
-                      .map((e) =>
-                          e.buildDrawerEntry(context, hasPath ? path! : ""))
+                      .map((e) => e.buildDrawerEntry(
+                          context, drawerTileStyle, hasPath ? path! : ""))
                       .whereType<Widget>()
                       .toList(),
                 ),
@@ -113,16 +117,40 @@ class BoringSection {
               controller: MaterialApp.createMaterialHeroController(),
               child: LayoutBuilder(builder: (context, constraints) {
                 return Scaffold(
-                  drawer: constraints.maxWidth > 750 ? null : drawer(context),
+                  key: _drawerKey,
+                  drawer: constraints.maxWidth > 750
+                      ? null
+                      : drawer(context, drawerShow: true),
                   body: constraints.maxWidth > 750
-                      ? Row(children: [
-                          Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: drawer(context),
+                      ? Padding(
+                          padding: drawerStyle.drawerForeignPadding,
+                          child: Row(children: [
+                            drawer(context),
+                            Expanded(child: child)
+                          ]),
+                        )
+                      : Padding(
+                          padding: drawerStyle.drawerForeignPadding.copyWith(
+                              top: drawerStyle.drawerForeignPadding.top,
+                              bottom: drawerStyle.drawerForeignPadding.bottom,
+                              right: drawerStyle.drawerForeignPadding.right,
+                              left: 0),
+                          child: Row(
+                            children: [
+                              Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(
+                                        left: 8, right: 8),
+                                    child: GestureDetector(
+                                        onTap: () => _drawerKey.currentState!
+                                            .openDrawer(),
+                                        child: drawerStyle.drawerIcon),
+                                  )),
+                              Expanded(child: child),
+                            ],
                           ),
-                          Expanded(child: child)
-                        ])
-                      : child,
+                        ),
                 );
               }),
             );
@@ -151,5 +179,5 @@ class BoringSection {
             noPathPage?.builder?.call(context, state) ?? const Placeholder(),
         routes: subRoutes());
   }
-  //TODO add drawer header and footer
+//TODO add drawer header and footer
 }
