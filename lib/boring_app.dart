631c9cfabd4093055de44576210ee24cf2d61895
enzo-desimone <en.desimone@outlook.it>
2023-04-18 13:59:17 +0200

    fix

--- lib/boring_app.dart
+++ lib/boring_app.dart
@@ -1,5 +1,7 @@
 library boring_app;
 
+export 'package:boring_app/boring_app/style/boring_drawer_style.dart';
+export 'package:boring_app/boring_app/boring_app.dart';
 export 'package:boring_app/boring_app/boring_page/boring_page.dart';
 export 'package:boring_app/boring_app/boring_drawer_entry.dart';
 export 'package:boring_app/boring_app/boring_app_section.dart';
