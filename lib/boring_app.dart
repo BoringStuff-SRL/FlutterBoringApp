c9aca0f5f4faf48d134a7ad47c882962c2a2b1b0
endes <en.desimone@outlook.it>
2023-10-24 11:30:13 +0200

    fix

--- lib/boring_app.dart
+++ lib/boring_app.dart
@@ -1,7 +1,9 @@
 library boring_app;
 
 export 'package:boring_app/boring_app.dart';
+export 'package:boring_app/boring_app/boring_app.dart';
 export 'package:go_router/go_router.dart';
 export 'package:boring_app/boring_app/pages/boring_page.dart';
 export 'package:boring_app/boring_app/navigation/navigation_entry.dart';
 export 'package:boring_app/boring_app/navigation/boring_navigation.dart';
+export 'package:boring_app/boring_app/navigation/drawer/boring_navigation_drawer.dart';
