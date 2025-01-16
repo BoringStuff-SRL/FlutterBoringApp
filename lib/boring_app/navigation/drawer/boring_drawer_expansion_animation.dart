87e664f604d4a5beb6a77bfd7fcb4351622a231b
alessiogianello <giane199@gmail.com>
2025-01-16 13:51:28 +0100

    _

--- lib/boring_app/navigation/drawer/boring_drawer_expansion_animation.dart
+++ lib/boring_app/navigation/drawer/boring_drawer_expansion_animation.dart
@@ -76,6 +76,7 @@ class _BoringDrawerExpansionAnimationState
 
   @override
   Widget build(BuildContext context) {
+    print('Drawer build');
     final children = <Widget>[];
 
     for (final group in widget.navigationGroups) {
